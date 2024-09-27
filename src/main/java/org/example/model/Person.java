0e778078d4e6e6e13402d1435300d05789584a19
webwanter <webwanter@gmail.com>
2024-09-27 08:10:09 +0500

    Подключение к бд

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -2,13 +2,14 @@ package org.example.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Person {
     private Long id;
     private String name;
     private String surname;
 
-    // Связь OneToMany (с классом Order)
+    // Связь ManyToMany (с классом Order)
     private List<Order> orders = new ArrayList<>();
 
 
@@ -40,5 +41,18 @@ public class Person {
         this.surname = surname;
         this.name = name;
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) && Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && Objects.equals(orders, person.orders);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, orders);
     }
 }
